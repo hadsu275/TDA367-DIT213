008452fce16a3904551208d89a8419b46ad8e9ac
Hadi <dinemail.se>
2025-11-14 13:48:13 +0100

    second commit

--- untitled/src/MazeGenerator.java
+++ untitled/src/MazeGenerator.java
@@ -28,6 +28,25 @@ public class MazeGenerator {
     public int getHeight() { return height; }
     public Cell getCell(int y, int x) { return maze[y][x]; }
 
+    private void openBorders() {
+        // Toppen
+        for (int x = 0; x < width; x++)
+            maze[0][x].walls[0] = false;     // N
+
+        // Botten
+        for (int x = 0; x < width; x++)
+            maze[height - 1][x].walls[1] = false; // S
+
+        // Vänster
+        for (int y = 0; y < height; y++)
+            maze[y][0].walls[3] = false;     // W
+
+        // Höger
+        for (int y = 0; y < height; y++)
+            maze[y][width - 1].walls[2] = false; // E
+    }
+
+
     public void generate() {
         Stack<int[]> stack = new Stack<>();
         int startY = random.nextInt(height);
@@ -50,8 +69,10 @@ public class MazeGenerator {
             if (!neighbors.isEmpty()) {
                 int[] chosen = neighbors.get(random.nextInt(neighbors.size()));
                 int ny = chosen[0], nx = chosen[1], wallCurrent = chosen[2], wallNeighbor = chosen[3];
-                maze[y][x].walls[wallCurrent] = false;
-                maze[ny][nx].walls[wallNeighbor] = false;
+                if (random.nextDouble() < densityFactor) {
+                    maze[y][x].walls[wallCurrent] = false;
+                    maze[ny][nx].walls[wallNeighbor] = false;
+                }
                 maze[ny][nx].visited = true;
                 stack.push(new int[]{ny, nx});
             } else {
